69584c27d8d7b73e242c587815bef3bee591da46
Yamato Tsugawa <tetsugakuman@gmail.com>
2025-08-19 08:52:26 +0900

    fix: use app router page.tsx and correct import path

--- src/app/freebusy/index.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const FreeBusyFinder = dynamic(() => import('../../components/FreeBusyFinder'), { ssr: false });
-
-export default function Page() {
-  return <FreeBusyFinder />;
-}
--- src/app/freebusy/page.tsx
+++ src/app/freebusy/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import FreeBusyFinder from './FreeBusyFinder';
 
 export default function Page() {
